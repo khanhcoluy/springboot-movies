67600221486251b64557cf3cf540a4024729221e
Khanh Nguyen <khanhcoluy@gmail.com>
2023-07-22 12:53:20 +0700

    - implement get all movies - implement get single movie

---
+++ src/main/java/codewithkhanh/movies/MovieController.java
@@ -0,0 +1,33 @@
+package codewithkhanh.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/movies")
+public class MovieController {
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        List<Movie> movies = movieService.getAllMovies();
+        return new ResponseEntity<>(movies, HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(
+            @PathVariable String imdbId
+    ) {
+        Optional<Movie> movie = movieService.getSingleMovie(imdbId);
+        return new ResponseEntity<>(movie, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/codewithkhanh/movies/MovieService.java
@@ -0,0 +1,21 @@
+package codewithkhanh.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> getSingleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
+}
